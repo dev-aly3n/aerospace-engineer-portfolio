a8ef444305a087c02d30c815df03df8fe13c362b
aly3n <aly3n.dev@gmail.com>
2021-08-21 16:13:20 +0430

    home page is mobile friendly now

--- components/homePage/Home.js
+++ components/homePage/Home.js
@@ -32,15 +32,14 @@ const Home = () => {
       {/* second */}
       {/* second */}
       {/* second */}
-      <div className="relative flex justify-around pb-24 py-5 mt-10">
-        <div className="absolute w-screen h-96 left-16 top-24 bg-gray-600 z-0"></div>
+      <div>
+        <div></div>
         <img
           src="/media/uni/seconduni.png"
-          className="w-[500px] ml-[150px] h-full z-10 rounded-3xl"
         />
-        <div className="flex flex-col">
+        <div className="text-and-title">
           <p
-            className="text-5xl font-black z-10 mt-20 text-white ml-5"
+            className=""
             style={{ lineHeight: 1.5 }}
           >
             I <span className="text-green-600">working</span> on UAVs
@@ -48,7 +47,7 @@ const Home = () => {
             Airplanes and <br />
             Drones...
           </p>
-          <p className="text-white z-10 w-72 ml-5">
+          <p className="">
             This is one of my team work artifact that we called it super drone
             plane. we worked on it for 3 month designing calculating and
             manufacturing. we made it on a team with 6 member at KNTU university

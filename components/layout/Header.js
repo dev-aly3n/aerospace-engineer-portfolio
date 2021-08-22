08a9018a1f979ae12fb793eec38db9fe8f72b663
aly3n <aly3n.dev@gmail.com>
2021-08-22 18:34:16 +0430

    header navbar added

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 const Header = () => {
 
     return (
-        <div className="h-16 w-full bg-gray-900"></div>
+        <div className="h-16 w-full bg-gray-900">
+            <ul className="flex justify-center items-center   text-xl font-bold gap-x-20">
+                <li >
+                    <Link href="/">
+                        <a className="text-white  hover:bg-gray-100 hover:text-black px-5 py-3 block">University</a>
+                    </Link>
+                </li>
+                <li >
+                    <Link href="/sport">
+                        <a className="text-white  hover:bg-gray-100 hover:text-black px-5 py-3 block">Sport</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 

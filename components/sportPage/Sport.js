75bae68a8df2bc321aeb5fbded96ba0ebca91d58
aly3n <aly3n.dev@gmail.com>
2021-08-23 11:43:11 +0430

    second section of sport now is mobile friendly

--- components/sportPage/Sport.js
+++ components/sportPage/Sport.js
@@ -15,51 +15,36 @@ const Sport = () => {
     });
   };
   return (
-    <div className="flex flex-col justify-center items-center w-full">
+    <div className="sport-container">
       {/* first */}
       {/* first */}
       {/* first */}
-      <div className="flex relative w-full justify-evenly items-center mt-10">
-        <div className="absolute top-10 left-5 sm:left-10 lg:left-36 xl:left-60 h-[469px] bg-yellow-400 w-full"></div>
-        <div className="z-10 flex xl:gap-x-20">
-          <p
-            className="z-10 text-[35px] font-black -mt-16"
-            style={{ writingMode: "vertical-lr", textOrientation: "upright" }}
-          >
-            TAEKWONDO
-          </p>
-          <img
-            className="h-[300px] z-10 hidden md:block"
-            src="/media/sport/sportfirst2.png"
-          />
+      <div>
+        <div></div>
+        <div>
+          <p>TAEKWONDO</p>
+          <img src="/media/sport/sportfirst2.png" />
         </div>
-        <img
-          className="h-[600px] z-10 "
-          src="/media/sport/sportfirst.png"
-          alt=""
-        />
+        <img src="/media/sport/sportfirst.png" alt="" />
       </div>
       {/* second */}
       {/* second */}
       {/* second */}
-      <div className="flex relative w-full justify-evenly items-center mt-32">
-        <div className="absolute top-10 right-5 sm:right-10 lg:right-36 xl:right-60 h-[400px] bg-gray-900 w-full"></div>
-        <img
-          className="h-[500px] z-10 border-r-8 border-t-4 rounded-3xl"
-          style={{ borderColor: "#FCCEC1" }}
-          src="/media/sport/sportsecond.png"
-          alt=""
-        />
-        <p className="z-10 text-[35px] text-white w-[480px] font-black mr-10">
+      <div>
+        <div></div>
+        <img src="/media/sport/sportsecond.png" alt="" />
+        <div>
+        <p style={{lineHeight:1.7}}>
           I am a professional athlete in{" "}
           <span className="text-yellow-400">Taekwondo</span> for more than 6
           years.
           <br />
-          <span className="text-lg font-medium">
+        </p>
+          <p>
             my mother is a Taekwondo master and she always encorage me to
             continue.
-          </span>
-        </p>
+          </p>
+          </div>
       </div>
 
       {/* third */}

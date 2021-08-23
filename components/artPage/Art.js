7730f9284465be9d72f4f4767f1f014b1fdc63b7
aly3n <aly3n.dev@gmail.com>
2021-08-23 20:58:11 +0430

    first section of art now is mobile friendly

--- components/artPage/Art.js
+++ components/artPage/Art.js
@@ -3,13 +3,14 @@ const Art = () => {
   console.log(musicNotes);
   return (
     <div className="w-full h-full flex flex-col bg-white art-container">
-      <div className="flex justify-between bg-pink-100">
+      <div className="flex flex-col ssm:flex-row justify-between bg-pink-100">
         <div
-          className="w-7/12 flex justify-center items-center text-7xl font-black relative"
+          className="h-52 -mb-12 ssm:h-auto ssm:w-1/2 md:w-7/12 flex justify-center items-center
+           text-4xl sm:text-5xl md:text-7xl font-black relative hover:scale-110 duration-1000 peer"
           style={{ color: "#307393" }}
         >
           <p> Daf is fun</p>
-          <div className="absolute flex justify-evenly w-screen top-72">
+          <div className="absolute flex gap-x-7 justify-evenly w-screen top-6 ssm:top-10 sm:top-20 md:top-32 lg:top-44 xl:top-64">
             {musicNotes.map((note) => {
               const randomNum = Math.random().toFixed(1) * 10;
               return (
@@ -17,7 +18,7 @@ const Art = () => {
                   className="h-8"
                   style={{
                     animation: `musicNote${randomNum % 5} ${
-                      (randomNum % 8) + 3
+                      (randomNum % 8) + 5
                     }s ease-out 0s infinite alternate`,
                   }}
                   src={`/media/art/musicnote${note % 3}.png`}
@@ -27,9 +28,10 @@ const Art = () => {
             })}
           </div>
         </div>
-        <div className="relative w-5/12 h-full">
+        <div className="relative ssm:w-1/2 md:w-5/12 h-full duration-1000 peer-hover:translate-x-10">
           <img
-            className="w-full h-full filter contrast-75 shadow-2xl  duration-700"
+            className="w-10/12 m-auto ssm:w-full h-full filter contrast-75 shadow-2xl rounded-md
+            hover:scale-110 ssm:rounded-none duration-1000 "
             src="/media/art/artfirst.jpg"
             alt=""
           />

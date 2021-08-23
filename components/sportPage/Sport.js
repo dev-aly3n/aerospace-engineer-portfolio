cf114837a70a9b6c434ac97dc0a31d5185f88bf0
aly3n <aly3n.dev@gmail.com>
2021-08-23 13:52:51 +0430

    third section added to sport page and now it is mobile friendly

--- components/sportPage/Sport.js
+++ components/sportPage/Sport.js
@@ -34,47 +34,40 @@ const Sport = () => {
         <div></div>
         <img src="/media/sport/sportsecond.png" alt="" />
         <div>
-        <p style={{lineHeight:1.7}}>
-          I am a professional athlete in{" "}
-          <span className="text-yellow-400">Taekwondo</span> for more than 6
-          years.
-          <br />
-        </p>
+          <p style={{ lineHeight: 1.7 }}>
+            I am a professional athlete in{" "}
+            <span className="text-yellow-400">Taekwondo</span> for more than 6
+            years.
+            <br />
+          </p>
           <p>
             my mother is a Taekwondo master and she always encorage me to
             continue.
           </p>
-          </div>
+        </div>
       </div>
 
       {/* third */}
       {/* third */}
       {/* third */}
-      <div className="flex relative w-full justify-evenly items-center mt-32 mb-80">
-        <div className="absolute top-10 left-5 sm:left-10 lg:left-36 xl:left-60 h-[400px] bg-gray-900 w-full"></div>
-        <img
-          className="h-[400px] z-10 shadow-2xl "
-          style={{ borderRadius: "0.5rem 1.5rem 8rem 0.9rem" }}
-          src="/media/sport/sportthird.jpg"
-          alt=""
-        />
-        <p className="z-10 text-4xl text-white w-[550px] font-black mr-10">
+      <div>
+        <div></div>
+
+        <p>
           I am NOT a big fan of beating people, so I focus just on{" "}
           <span className="text-yellow-400">Taekwondo Poomsae</span> <br />
-          <span className="text-xl font-medium">
+          <span className="text-lg lg:text-xl font-normal lg:font-medium">
             There is a world of peace for me in Taekwondo, I use it to clean my
             brain and make my body healthy. I have many{" "}
             <span className="text-yellow-400">Medals</span> in Poomsae
-            competetions that you can see them
-            <button
-              onClick={atTheFollowingHandler}
-              className="text-blue-300 hover:scale-105 duration-700 hover:text-blue-600 font-black"
-            >
-              at the following!{" "}
+            competetions that you can see them{" "}
+            <button onClick={atTheFollowingHandler}>
+              at the following!
               <FontAwesomeIcon className="animate-bounce" icon={faArrowDown} />
             </button>
           </span>
         </p>
+        <img src="/media/sport/sportthird.jpg" alt="" />
       </div>
 
       {/* achievement */}

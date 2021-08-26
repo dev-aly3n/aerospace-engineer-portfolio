cc5432c246e3260240b119590faea9310e8bacc4
aly3n <aly3n.dev@gmail.com>
2021-08-26 11:25:12 +0430

    art page inline css transfered to different css file

--- components/artPage/Art.js
+++ components/artPage/Art.js
@@ -21,23 +21,18 @@ const Art = () => {
     }
   };
   return (
-    <div className="w-full h-full flex flex-col bg-blue-50 art-container">
+    <div className="art-container">
       {/* first */}
       {/* first */}
       {/* first */}
-      <div className="flex flex-col ssm:flex-row justify-between bg-pink-100 overflow-hidden shadow-lg">
-        <div
-          className="h-52 -mb-12 ssm:h-auto ssm:w-1/2 md:w-7/12 flex justify-center items-center
-           text-4xl sm:text-5xl md:text-7xl font-black relative hover:scale-110 duration-1000 peer"
-          style={{ color: "#307393" }}
-        >
+      <div>
+        <div className="peer">
           <p> Daf is fun</p>
-          <div className="absolute flex gap-x-7 justify-evenly w-screen top-6 ssm:top-10 sm:top-20 md:top-32 lg:top-44 xl:top-64">
+          <div>
             {musicNotes.map((note) => {
               const randomNum = Math.random().toFixed(1) * 10;
               return (
                 <img
-                  className="h-8 "
                   style={{
                     animation: `musicNote${randomNum % 5} ${
                       (randomNum % (showBtn ? 8 : 1)) + (showBtn ? 5 : 1)
@@ -51,7 +46,7 @@ const Art = () => {
           </div>
         </div>
         <div
-          className={`relative ssm:w-1/2 md:w-5/12 h-full duration-2000 shadow-lg cursor-pointer
+          className={`
           ${
             showBtn
               ? "md:peer-hover:translate-x-10 filter peer-hover:grayscale peer-hover:blur-ssm"
@@ -59,15 +54,8 @@ const Art = () => {
           }`}
           onClick={firstVideoClickHandler}
         >
-          <div
-            className={`${
-              showBtn ? "flex" : "hidden"
-            }  absolute justify-center items-center w-full h-full text-7xl text-pink-100/75 z-10 peer`}
-          >
-            <FontAwesomeIcon
-              className="hover:text-pink-200 duration-700"
-              icon={faPlayCircle}
-            />
+          <div className={`${showBtn ? "flex" : "hidden"}   peer`}>
+            <FontAwesomeIcon className="font-icon-class" icon={faPlayCircle} />
           </div>
           <video
             ref={firstVideoRef}
@@ -75,9 +63,7 @@ const Art = () => {
             loop
             poster={"/media/art/artfirst.jpg"}
             width="500px"
-            className={`${
-              showBtn ? "peer-hover:scale-110" : ""
-            } w-10/12 m-auto ssm:w-full h-full filter contrast-75 shadow-2xl rounded-md ssm:rounded-none duration-1500`}
+            className={`${showBtn ? "peer-hover:scale-110" : ""} `}
             alt=""
           >
             <source src={"/media/art/artvideofirst.mp4"} type="video/mp4" />
@@ -88,12 +74,10 @@ const Art = () => {
       {/* second */}
       {/* second */}
 
-      <div className="flex flex-col justify-center items-center h-full mt-32 mx-auto w-11/12 ssm:w-10/12 ">
-        <h3 className=" text-3xl font-bold self-start mb-8 md:mb-0">
-          About Daf
-        </h3>
-        <div className="flex flex-col md:flex-row justify-evenly items-center bg-blue-50 ">
-          <div className="flex flex-col gap-y-5 sm:gap-y-10 justify-evenly items-start text-base lg:text-xl w-11/12 md:w-1/2">
+      <div>
+        <h3>About Daf</h3>
+        <div>
+          <div>
             <p>
               <FontAwesomeIcon icon={faMusic} /> Daf is a Middle Eastern frame
               drum musical instrument.
@@ -111,19 +95,15 @@ const Art = () => {
               and suddenly released (like the action of finger-snapping) to
               produce loud, rapid, sharp sounds.
             </p>
-            <p className="font-semibold lg:text-xl">
+            <p>
               <FontAwesomeIcon icon={faMusic} /> Playing Daf is a hobby for me
               for filling my time by beauty of music. It's about 4 years that I
               play Daf. I training once a week and had participate in some
               festivals.
             </p>
           </div>
-          <div className="ssm:w-[400px] mt-7 md:mt-0 rounded-md overflow-hidden ssm:hover:translate-x-3 duration-1500 shadow-neu1 ">
-            <img
-              className="w-full hover:scale-105 hover:rotate-2 duration-1500 "
-              src="media/art/artsecond.jpg"
-              alt=""
-            />
+          <div>
+            <img src="media/art/artsecond.jpg" alt="" />
           </div>
         </div>
       </div>
@@ -131,11 +111,11 @@ const Art = () => {
       {/* third */}
       {/* third */}
       {/* third */}
-      <div className="flex flex-col ssm:flex-row justify-center items-center bg-blue-50 h-full mx-auto w-10/12 relative mt-20">
-        <div className="w-[400px] self-start mt-32 hidden md:block">
+      <div>
+        <div>
           <img src="media/art/artthird.png" alt="" />
         </div>
-        <div className="ssm:w-10/12 ">
+        <div>
           <div ref={achieveRef} className="achievement ">
             <h2 className="!text-black !font-bold">My Achievements</h2>
             <div className="!bg-black"></div>

ce91c0878e2905f697dd738f28ee356f7b822652
aly3n <aly3n.dev@gmail.com>
2021-09-11 08:44:04 +0430

    some meta tag added to head for seo reasons

--- pages/biography/index.js
+++ pages/biography/index.js
@@ -6,6 +6,13 @@ export default function biography() {
   return (
     <Fragment>
       <Head>
+      <title>Atefeh Hasani - Biography</title>
+        <meta
+          name="description"
+          content="Atefeh Hasani is an aerospace engineer and a pro taekwondo athlete who is also good in music."
+        />
+        <meta name="keywords" content="Atefeh Hasani , عاطفه حسنی" />
+        <meta name="author" content="Atefeh Hasani" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
--- pages/sport/index.js
+++ pages/sport/index.js
@@ -6,6 +6,13 @@ export default function sport() {
   return (
     <Fragment>
       <Head>
+      <title>Atefeh Hasani - Sport</title>
+        <meta
+          name="description"
+          content="Atefeh Hasani is an aerospace engineer and a pro taekwondo athlete who is also good in music."
+        />
+        <meta name="keywords" content="Atefeh Hasani , عاطفه حسنی" />
+        <meta name="author" content="Atefeh Hasani" />
         <link rel="preload" as="image" href="/media/uni/unifirst.png" />
         <link rel="preload" as="image" href="/media/art/artfirst.jpg" />
         <link rel="preload" as="image" href="/media/sport/sportfirst.png" />

5dd4c115a556be4504a2f338a1356acc6966d8fc
aly3n <aly3n.dev@gmail.com>
2021-12-14 18:46:55 +0330

    alt tag added

--- components/Achievement.js
+++ components/Achievement.js
@@ -65,7 +65,7 @@ const Achievement = ({ achieve, light, num }) => {
             {achieve.image2 && (
               <Image
                 src={achieve.image2}
-                alt=""
+                alt={"atefeh hasani - " + achieve.title}
                 placeholder="blur"
                 blurDataURL={`data:image/svg+xml;base64,${toBase64(
                   shimmer(700, 475)
